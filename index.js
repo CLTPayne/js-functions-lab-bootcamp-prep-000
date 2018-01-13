3b1315826e333860e3bab6b02871eb44810752bf
Charlotte Payne <cltpayne@gmail.com>
2018-01-13 15:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ function happyHolidays(){
   return greetings;
 }
 
-function happyHolidaysTo(){
+function happyHolidaysTo(nqme){
   var name = `Happy holidays, ${name}!`;
   return name;
+}
+
+function holidayCountdown(holiday, days) {
+  return `It\'s ${days} days until ${holiday}!`;
 }
